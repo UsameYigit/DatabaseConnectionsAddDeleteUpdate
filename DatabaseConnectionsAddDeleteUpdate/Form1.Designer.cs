8b52aa0fd8e56452a2735947e7c308377723ae1f
Usame Yiğit <erusame@hotmail.com>
2022-02-06 03:27:14 +0300

    Add project files.

---
+++ DatabaseConnectionsAddDeleteUpdate/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace DatabaseConnectionsAddDeleteUpdate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvShippers = new System.Windows.Forms.ListView();
+            this.clmnShipperID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmnCompanyName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.clmnPhone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtShipperName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lblEditingShippingPanel = new System.Windows.Forms.Label();
+            this.lblShipperName = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvShippers
+            // 
+            this.lvShippers.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.clmnShipperID,
+            this.clmnCompanyName,
+            this.clmnPhone});
+            this.lvShippers.FullRowSelect = true;
+            this.lvShippers.HideSelection = false;
+            this.lvShippers.Location = new System.Drawing.Point(40, 44);
+            this.lvShippers.MultiSelect = false;
+            this.lvShippers.Name = "lvShippers";
+            this.lvShippers.Size = new System.Drawing.Size(267, 279);
+            this.lvShippers.TabIndex = 0;
+            this.lvShippers.UseCompatibleStateImageBehavior = false;
+            this.lvShippers.View = System.Windows.Forms.View.Details;
+            // 
+            // clmnShipperID
+            // 
+            this.clmnShipperID.Text = "ShipperID";
+            // 
+            // clmnCompanyName
+            // 
+            this.clmnCompanyName.Text = "Company Name";
+            // 
+            // clmnPhone
+            // 
+            this.clmnPhone.Text = "Phone";
+            // 
+            // txtShipperName
+            // 
+            this.txtShipperName.Location = new System.Drawing.Point(430, 86);
+            this.txtShipperName.Name = "txtShipperName";
+            this.txtShipperName.Size = new System.Drawing.Size(129, 20);
+            this.txtShipperName.TabIndex = 1;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(430, 122);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(129, 20);
+            this.txtPhone.TabIndex = 2;
+            // 
+            // lblEditingShippingPanel
+            // 
+            this.lblEditingShippingPanel.AutoSize = true;
+            this.lblEditingShippingPanel.Location = new System.Drawing.Point(439, 44);
+            this.lblEditingShippingPanel.Name = "lblEditingShippingPanel";
+            this.lblEditingShippingPanel.Size = new System.Drawing.Size(108, 13);
+            this.lblEditingShippingPanel.TabIndex = 3;
+            this.lblEditingShippingPanel.Text = "Editing Shipper Panel";
+            // 
+            // lblShipperName
+            // 
+            this.lblShipperName.AutoSize = true;
+            this.lblShipperName.Location = new System.Drawing.Point(347, 89);
+            this.lblShipperName.Name = "lblShipperName";
+            this.lblShipperName.Size = new System.Drawing.Size(77, 13);
+            this.lblShipperName.TabIndex = 4;
+            this.lblShipperName.Text = "Shipper Name:";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(347, 125);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(41, 13);
+            this.lblPhone.TabIndex = 5;
+            this.lblPhone.Text = "Phone:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(430, 175);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(129, 33);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(430, 235);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(129, 33);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(430, 290);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(129, 33);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 352);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblShipperName);
+            this.Controls.Add(this.lblEditingShippingPanel);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtShipperName);
+            this.Controls.Add(this.lvShippers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvShippers;
+        private System.Windows.Forms.ColumnHeader clmnShipperID;
+        private System.Windows.Forms.ColumnHeader clmnCompanyName;
+        private System.Windows.Forms.ColumnHeader clmnPhone;
+        private System.Windows.Forms.TextBox txtShipperName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lblEditingShippingPanel;
+        private System.Windows.Forms.Label lblShipperName;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ DatabaseConnectionsAddDeleteUpdate/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DatabaseConnectionsAddDeleteUpdate
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("Server = .; Database = Northwind; UID = sa; PWD = 12345");
+        SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ShipperAdd();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            ShipperUpdate();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            ShipperDelete();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetListView();
+        }
+
+        void ShipperAdd()
+        {
+            if (string.IsNullOrEmpty(txtPhone.Text) || string.IsNullOrEmpty(txtShipperName.Text))
+                MessageBox.Show("You need to fill the Shipper Name and Phone in order to add the shipper.");
+            else
+            {
+                cmd = new SqlCommand("INSERT INTO Shippers VALUES (@CompanyName, @Phone)", con);
+
+                cmd.Parameters.AddWithValue("@CompanyName", txtShipperName.Text);
+                cmd.Parameters.AddWithValue("@Phone", txtPhone.Text);
+
+                CommandExecution();
+
+                SetListView();
+            }
+            ResetTextFields();
+        }
+
+        void ShipperUpdate()
+        {
+            if (lvShippers.SelectedItems.Count == 0 || (string.IsNullOrEmpty(txtPhone.Text) && string.IsNullOrEmpty(txtShipperName.Text)))
+                MessageBox.Show("You need to fill the Shipper Name and Phone at the same time and you have to select a shipper to update.");
+            else
+            {
+                if (string.IsNullOrEmpty(txtPhone.Text))
+                    cmd = new SqlCommand("UPDATE Shippers SET CompanyName = @CompanyName WHERE ShipperID = @ShipperID", con);
+                else if (string.IsNullOrEmpty(txtShipperName.Text))
+                    cmd = new SqlCommand("UPDATE Shippers SET Phone = @Phone WHERE ShipperID = @ShipperID", con);
+                else
+                    cmd = new SqlCommand("UPDATE Shippers SET CompanyName = @CompanyName, Phone = @Phone WHERE ShipperID = @ShipperID", con);
+
+                cmd.Parameters.AddWithValue("@CompanyName", txtShipperName.Text);
+                cmd.Parameters.AddWithValue("@Phone", txtPhone.Text);
+                cmd.Parameters.AddWithValue("@ShipperID", lvShippers.SelectedItems[0].SubItems[0].Text);
+
+                CommandExecution();
+            }
+            ResetTextFields();
+        }
+
+        void ShipperDelete()
+        {
+            if (lvShippers.SelectedItems.Count == 0)
+                MessageBox.Show("You have to choose a Shipper to delete.");
+            else
+            {
+                cmd = new SqlCommand("DELETE FROM Shippers WHERE ShipperID = @ShipperID", con);
+
+                cmd.Parameters.AddWithValue("@ShipperID", lvShippers.SelectedItems[0].SubItems[0].Text);
+
+                CommandExecution();
+
+            }
+            ResetTextFields();
+        }
+
+        void ResetTextFields()
+        {
+            txtPhone.Text = "";
+            txtShipperName.Text = "";
+        }
+
+        void CommandExecution()
+        {
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+
+                int rowsEffected = cmd.ExecuteNonQuery();
+                MessageBox.Show(rowsEffected + " row(s) effected!");
+            }
+            catch (SqlException se)
+            {
+                MessageBox.Show(se.Message);
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show(e1.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            SetListView();
+        }
+
+        void SetListView()
+        {
+            lvShippers.Items.Clear();
+            cmd = new SqlCommand("SELECT * FROM Shippers", con);
+            try
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        ListViewItem lvi = new ListViewItem(reader.GetInt32(0).ToString());
+                        lvi.SubItems.Add(reader.GetString(1));
+                        lvi.SubItems.Add(reader.GetString(2));
+                        lvShippers.Items.Add(lvi);
+                    }
+                }
+            }
+            catch (SqlException se)
+            {
+                MessageBox.Show(se.Message);
+            }
+            catch (Exception e1)
+            {
+                MessageBox.Show(e1.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+            lvShippers.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+    }
+}
